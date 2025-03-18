9ac0e10b15800b27281bec9ea1ce64abe5a62a59
techpro-aws-devops <154894108+techpro-aws-devops@users.noreply.github.com>
2025-03-18 11:37:35 +0300

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 hostnamectl set-hostname ${server-name}
-dnf update -y
-dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-# curl -fsSL https://get.docker.com -o get-docker.sh
-# sh get-docker.sh
+apt update -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
 groupadd docker
 usermod -aG docker $USER
 newgrp docker
