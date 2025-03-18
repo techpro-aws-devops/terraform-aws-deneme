c73eb9adbb88cc1dd7092a7d1b40371cdc9abad1
Ubuntu <ubuntu@ip-172-31-85-207.ec2.internal>
2025-03-18 09:08:49 +0000

    should define your key file

--- userdata.sh
+++ userdata.sh
@@ -2,7 +2,7 @@
 hostnamectl set-hostname ${server-name}
 apt update -y
 curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-groupadd docker
-usermod -aG docker $USER
+sudo sh get-docker.sh
+sudo groupadd docker
+sudo usermod -aG docker $USER
 newgrp docker
